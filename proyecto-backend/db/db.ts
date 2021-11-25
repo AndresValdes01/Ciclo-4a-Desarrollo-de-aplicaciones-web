24ce0a51d348dbe946cbfd76bc96e715c43753a4
JuanChaves91 <jlchavesb30@gmail.com>
2021-11-25 10:04:07 -0500

    se agregan dependencias express, cors, graphql, apollo-server, dotenv y se mejora la seguridad de la conexión a la bd

--- proyecto-backend/db/db.ts
+++ proyecto-backend/db/db.ts
@@ -1,7 +1,7 @@
 import { connect } from 'mongoose';
 
 const conectarBD = async () => {
-    return await connect('mongodb+srv://admin:admin@gestionproyecto.epcd8.mongodb.net/GestionProyectos?retryWrites=true&w=majority')
+    return await connect(process.env.DATABASE_URL)
         .then(() => {
             console.log('Conexión exitosa a la BD');
         })
--- proyecto-backend/index.ts
+++ proyecto-backend/index.ts
@@ -1,7 +1,27 @@
 import conectarBD from "./db/db";
+import express from 'express';
+import cors from 'cors';
+import {ApolloServer} from 'apollo-server-express';
+import dotenv from 'dotenv';
 
-const main = async () => {
+dotenv.config();
+
+//const server = new ApolloServer({
+//    typeDefs:'',
+//    resolvers:'',
+//})
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.listen({port:process.env.PORT || 4000}, async ()=>{
     await conectarBD();
-}
+    //await server.start();
 
-main();
+    //server.applyMiddleware({app});
+
+    console.log('Servidor listo');
+})
