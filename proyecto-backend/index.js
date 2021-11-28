27d8492480a4a68ed86a8e916cfe1a1adf3f54fc
JuanChaves91 <jlchavesb30@gmail.com>
2021-11-28 08:23:12 -0500

    modelo inscripcion

--- proyecto-backend/index.js
+++ proyecto-backend/index.js
@@ -3,13 +3,15 @@ import express from 'express';
 import cors from 'cors';
 import {ApolloServer} from 'apollo-server-express';
 import dotenv from 'dotenv';
+import { tipos } from './graphql/types.js';
+import { resolvers } from './graphql/resolvers.js';
 
 dotenv.config();
 
-//const server = new ApolloServer({
-//    typeDefs:'',
-//    resolvers:'',
-//})
+const server = new ApolloServer({
+    typeDefs:tipos,
+    resolvers:resolvers,
+})
 
 const app = express();
 
@@ -19,9 +21,9 @@ app.use(cors());
 
 app.listen({port:process.env.PORT || 4000}, async ()=>{
     await conectarBD();
-    //await server.start();
+    await server.start();
 
-    //server.applyMiddleware({app});
+    server.applyMiddleware({app});
 
     console.log('Servidor listo');
 })
