28e792ba863dee496ae03e6ac3c40bc60ed3373e
JuanChaves91 <jlchavesb30@gmail.com>
2021-12-07 09:32:08 -0500

    Importar módulos de ApolloClient

---
+++ proyecto-frontend/src/App.jsx
@@ -0,0 +1,18 @@
+//import logo from './logo.svg';
+//import './App.css';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache()
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+
+    </ApolloProvider>
+  );
+}
+
+export default App;
