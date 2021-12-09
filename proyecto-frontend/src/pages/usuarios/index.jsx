d75670c1135dd4324535ad3e76205ecb73de3786
JuanChaves91 <90256152+JuanChaves91@users.noreply.github.com>
2021-12-09 16:13:48 -0500

    Update index.jsx

--- proyecto-frontend/src/pages/usuarios/index.jsx
+++ proyecto-frontend/src/pages/usuarios/index.jsx
@@ -48,7 +48,7 @@ const IndexUsuarios = () => {
                   <td>{Enum_EstadoUsuario[u.estado]}</td>
                   <td>
                     <Link to={`/usuarios/editar/${u._id}`}>
-                      <i className='fas fa-pen text-yellow-600 hover:text-yellow-400 cursor-pointer' />
+                      <i className='fas fa-pen text-yellow-600 hover:text-blue-400 cursor-pointer' />
                     </Link>
                   </td>
                 </tr>
