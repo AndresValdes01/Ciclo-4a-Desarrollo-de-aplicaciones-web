5a21697a04ce23f4c274997c839a8f407c227991
JuanChaves91 <jlchavesb30@gmail.com>
2021-12-13 10:21:44 -0500

    Validación frontend

--- proyecto-frontend/src/components/Sidebar.jsx
+++ proyecto-frontend/src/components/Sidebar.jsx
@@ -1,20 +1,36 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { useAuth } from 'context/authContext';
+import PrivateComponent from './PrivateComponent';
 
-const SidebarRoute = ({ to, title, icon }) => {
+const SidebarLinks = () => {
   return (
-    <li>
-      <NavLink
-        to={to}
-        className={({ isActive }) =>
-          isActive
-            ? 'sidebar-route text-white bg-indigo-700'
-            : 'sidebar-route text-gray-900 hover:text-white hover:bg-indigo-400'
-        }
-      >
+    <ul className='mt-12'>
+      <SidebarRoute to='' title='Inicio' icon='fas fa-home' />
+      <PrivateComponent roleList={['ADMINISTRADOR']}>
+        <SidebarRoute to='/usuarios' title='Usuarios' icon='fas fa-user' />
+      </PrivateComponent>
+      <SidebarRoute to='/proyectos' title='Proyectos' icon='fas fa-smile-wink' />
+      <PrivateComponent roleList={['ADMINISTRADOR', 'LIDER']}>
+        <SidebarRoute to='/inscripciones' title='Aprobacion Inscripciones' icon='fas fa-user' />
+      </PrivateComponent>
+      <Logout />
+    </ul>
+  );
+};
+
+const Logout = () => {
+  const { setToken } = useAuth();
+  const deleteToken = () => {
+    console.log('eliminar token');
+    setToken(null);
+  };
+  return (
+    <li onClick={() => deleteToken()}>
+      <NavLink to='/auth/login' className='sidebar-route text-red-700'>
         <div className='flex items-center'>
-          <i className={icon} />
-          <span className='text-sm  ml-2'>{title}</span>
+          <i className='fas fa-sign-out-alt' />
+          <span className='text-sm  ml-2'>Cerrar Sesión</span>
         </div>
       </NavLink>
     </li>
@@ -24,8 +40,30 @@ const SidebarRoute = ({ to, title, icon }) => {
 const Logo = () => {
   return (
     <div className='py-3 w-full flex flex-col items-center justify-center'>
-      <img src='logo.png' alt='Logo' className='h-16 rounded-full' />
-      <span className='my-2 text-xl font-bold text-center'>Gestión de Proyectos</span>
+      <img src='logo.png' alt='Logo' className='h-16' />
+      <span className='my-2 text-xl font-bold text-center'>Gestión de proyectos</span>
+    </div>
+  );
+};
+
+const Sidebar = () => {
+  const [open, setOpen] = useState(true);
+  return (
+    <div className='flex flex-col md:flex-row flex-no-wrap md:h-full'>
+      {/* Sidebar starts */}
+
+      <div className='sidebar hidden md:flex'>
+        <div className='px-8'>
+          <Logo />
+          <SidebarLinks />
+        </div>
+      </div>
+      <div className='flex md:hidden w-full justify-between bg-gray-800 p-2 text-white'>
+        <i className={`fas fa-${open ? 'times' : 'bars'}`} onClick={() => setOpen(!open)} />
+        <i className='fas fa-home' />
+      </div>
+      {open && <ResponsiveSidebar />}
+      {/* Sidebar ends */}
     </div>
   );
 };
@@ -46,34 +84,23 @@ const ResponsiveSidebar = () => {
   );
 };
 
-const SidebarLinks = () => {
-    return (
-      <ul className='mt-12'>
-        <SidebarRoute to='' title='Inicio' icon='fas fa-home' />
-        <SidebarRoute to='/usuarios' title='Usuarios' icon='fas fa-user' />
-      </ul>
-    )
-}
-
-const Sidebar = () => { 
-  const [open, setOpen] = useState(true);
+const SidebarRoute = ({ to, title, icon }) => {
   return (
-    <div className='flex flex-col md:flex-row flex-no-wrap md:h-full'>
-      {/* Sidebar starts */}
-
-      <div className='sidebar hidden md:flex'>
-        <div className='px-8'>
-          <Logo />
-          <SidebarLinks />
+    <li>
+      <NavLink
+        to={to}
+        className={({ isActive }) =>
+          isActive
+            ? 'sidebar-route text-white bg-indigo-700'
+            : 'sidebar-route text-gray-900 hover:text-white hover:bg-indigo-400'
+        }
+      >
+        <div className='flex items-center'>
+          <i className={icon} />
+          <span className='text-sm  ml-2'>{title}</span>
         </div>
-      </div>
-      <div className='flex md:hidden w-full justify-between bg-gray-800 p-2 text-white'>
-        <i className={`fas fa-${open ? 'times' : 'bars'}`} onClick={() => setOpen(!open)} />
-        <i className='fas fa-home' />
-      </div>
-      {open && <ResponsiveSidebar />}
-      {/* Sidebar ends */}
-    </div>
+      </NavLink>
+    </li>
   );
 };
 
